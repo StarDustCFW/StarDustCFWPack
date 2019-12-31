56b3df3a3d9e37ed05cc4e0e059c0586b9fde4a3
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2019-12-31 13:10:39 +0000

    update

---
+++ For PC/Abreme.bat
@@ -0,0 +1,23 @@
+@echo off
+title Descargando utilidades
+mode con:lines=4 cols=52
+echo Obteninedo lista
+wget http://arte-tian-cuba.000webhostapp.com/net/PC.txt
+for /f "delims= " %%i in (PC.txt) do (
+start /MIN wget %%i
+)
+
+cls
+echo Descargando en segundo plano
+
+:1
+for /f "delims= " %%i in ('tasklist /fi "imagename eq wget.exe"') do (set e=%%i)
+if /i "%e%" neq "wget.exe" goto:2
+%windir%\system32\timeout 01>nul
+goto:1
+
+:2
+echo.
+echo Todo Descargado
+echo.
+pause
