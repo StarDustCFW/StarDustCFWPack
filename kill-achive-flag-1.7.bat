76d50cccf6b821ddc9f1d9aedfd7f8a3228ab829
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2020-05-14 01:53:27 +0100

    update

---
+++ kill-achive-flag-1.7.bat
@@ -0,0 +1,50 @@
+@echo off
+set ver=1.7
+rem by Kronos2308
+rename %0 kill-achive-flag-%ver%.bat
+title yo busco la sd solita ponme donde quieras ver %ver%
+color 0a
+set azucar=false
+echo ----------->%temp%\loger
+echo LOG>>%temp%\loger
+echo ----------->>%temp%\loger
+
+for /D %%i in (D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+set l=%%i
+rem if exist %%i:\Nintendo goto:Work
+
+
+	if exist %%i:\Nintendo (
+		echo Encontre la sd en %%i:\ >>%temp%\loger
+		echo Encontre la sd en %%i:\
+		echo Buscando errores en %%i:
+		CHKDSK /f %%i:
+		echo -------------------------
+		echo Arreglando flag de archivo en %%i:
+		echo Arreglando flag de archivo en %%i:>>%temp%\loger
+		attrib -s -h -a %%i:\* /S /D>>%temp%\loger
+		attrib +a %%i:\emutendo\Contents\registered\* /S /D
+		attrib +a %%i:\Nintendo\Contents\registered\* /S /D>>%temp%\loger
+		set azucar=true
+	)
+)
+
+:Work
+set l=d
+set con=0
+type %temp%\loger
+echo -------
+color 0a
+if %azucar% == false (
+echo NO ENCONTRE LA SD, Donde esta?
+color 04
+)
+
+echo termine todo
+
+%windir%\system32\timeout.exe 150
+exit
+
+
+
+
