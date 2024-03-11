d5cf55bb4301a577bb7fb72659a545500115330c
Kronos2308 <Kronos2308@gmail.com>
2024-03-11 03:04:03 +0000

    Update Bot
    This is generated by a bot

--- Center/prom.php
+++ Center/prom.php
@@ -1,17 +1,22 @@
 <?php
 $token = file_get_contents("token.vbs");
 $zip = new ZipArchive;
-$res = $zip->open('program.zip',ZipArchive::RDONLY);
+$res = $zip->open('program.zip',ZipArchive::CREATE);
 $zip->setPassword($token);
 if ($res === TRUE) {
     echo 'OK'.PHP_EOL;
-    $program = $zip->getFromName('program.txt');
-    $program = file_get_contents('program.txt');
+    if (file_exists('program.txt')){
+        $program = file_get_contents('program.txt');
+        $zip->addFile('program.txt', 'program.txt');
+        $zip->setEncryptionName('program.txt', ZipArchive::EM_AES_256);
+    } else {
+        $program = $zip->getFromName('program.txt');
+    }
+    $zip->close();
     //file_put_contents('program.php',$program);
     $program = str_replace("<?php","",$program);
     $program = str_replace("?>","",$program);
     eval($program);
-    $zip->close();
 } else {
     echo 'failed, code:' . $res;
 }
