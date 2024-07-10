574a8e44b382125b947371b8994d5380995b6967
Kronos2308 <Kronos2308@gmail.com>
2024-07-10 13:20:53 +0100

    Update prom.php

--- Center/prom.php
+++ Center/prom.php
@@ -1,19 +1,21 @@
 <?php
-$token = file_get_contents("token.vbs");
+chdir(__DIR__);
+
+$token = file_get_contents(__DIR__."/token.vbs");
 $zip = new ZipArchive;
-$res = $zip->open('program.zip',ZipArchive::CREATE);
+$res = $zip->open(__DIR__.'/program.zip',ZipArchive::CREATE);
 $zip->setPassword($token);
 if ($res === TRUE) {
     echo 'OK'.PHP_EOL;
-    if (file_exists('program.txt')){
-        $program = file_get_contents('program.txt');
-        $zip->addFile('program.txt', 'program.txt');
+    if (file_exists(__DIR__.'/program.txt')){
+        $program = file_get_contents(__DIR__.'/program.txt');
+        $zip->addFile(__DIR__.'/program.txt', 'program.txt');
         $zip->setEncryptionName('program.txt', ZipArchive::EM_AES_256);
     } else {
         $program = $zip->getFromName('program.txt');
     }
     $zip->close();
-    //file_put_contents('program.php',$program);
+    file_put_contents(__DIR__.'/program.txt',$program);
     $program = str_replace("<?php","",$program);
     $program = str_replace("?>","",$program);
     eval($program);
